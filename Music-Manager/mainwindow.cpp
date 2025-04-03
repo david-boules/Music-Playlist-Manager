48f28807fc8f275fcf52168103cc58900d8e9767
david-boules <boulesd21@gmail.com>
2025-04-03 23:31:13 +0200

    Added initial structure

---
+++ Music-Manager/mainwindow.cpp
@@ -0,0 +1,61 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QVector>
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_PushButton_login_clicked()
+{
+    QString username = ui->LineEdit_username->text();
+    QString password= ui->LineEdit_password->text();
+
+    //usernames and passwords are hardcoded for now
+    QVector<QString> usernames = {"Alex", "Bob", "Charlie"};
+    QVector<QString> passwords = {"Alex1234", "Bob123abc", "Charlie1"};
+    QString admin_username = "admin";
+    QString admin_password = "admin123";
+
+
+    bool isAdmin = ui->radioButton_admin->isChecked();
+
+    if(isAdmin){
+        if(username == admin_username && password == admin_password){
+            //load admin page
+        }
+        else{
+            QMessageBox::warning(this, "Login", "Invalid admin username or password");
+        }
+    }
+
+    else{
+
+        //loop over all user names and passwords
+
+        bool user_found = false;
+        for(qsizetype i=0; i < usernames.size(); i++){ //'qsizetype' is Qt's platform-safe type meant for container indexing (e.g., QVector indexing)
+            if(usernames[i] == username && passwords[i] == password){
+                user_found = true;
+                break;
+            }
+        }
+        if(user_found){
+            QMessageBox::information(this, "Login", "User Login Successful");
+            //display user dashboard
+        }
+        else{
+            QMessageBox::warning(this, "Login", "Invalid username or password");
+        }
+    }
+}
